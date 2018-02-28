6f6572d59ae96169eb2a31eedb3c02a7f78b1db2
Albert Patera <albertpatera@seznam.cz>
2018-02-28 22:35:22 +0100

    save project before use function

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,15 @@
-$("fieldset").mouseover(function() {
+
+
+var getAnimation = function () {
+	$("fieldset").mouseover(function() {
   		$("fieldset").fadeOut(750);
   		$("fieldset").css("background-color", "black");
   		$("fieldset").css("opacity", "0.9");
   		$("fieldset").fadeIn(750);
 
 })
+
+}
+
+setTimeout(function(){ 
+	get }, 3000);
